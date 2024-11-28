462ecf35ff19ac769bf4cdef697fce43a5ebd2d1
Tyuiu.AvdeevAS <Tyuiu.AvdeevAS@outlook.com>
2024-11-29 02:55:32 +0500

    fix

--- Tyuiu.AvdeevAS.Sprint5.Task4.V5.Lib/DataService.cs
+++ Tyuiu.AvdeevAS.Sprint5.Task4.V5.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.AvdeevAS.Sprint5.Task4.V5.Lib
 {
     public class DataService : ISprint5Task4V5
@@ -12,18 +13,15 @@ namespace Tyuiu.AvdeevAS.Sprint5.Task4.V5.Lib
 
             // Чтение значения из файла
             string fileContent = File.ReadAllText(path);
-            if (!double.TryParse(fileContent, out double x))
-            {
-                throw new InvalidDataException("Файл содержит некорректные данные.");
-            }
+
+           double x = double.Parse(fileContent);
 
             // Проверка на деление на ноль
             double denominator = Math.Sin(x);
-            if (Math.Abs(denominator) < 1e-10)
-            {
-                throw new DivideByZeroException("Знаменатель равен нулю.");
-            }
+            
 
+            
+            
             // Вычисление значения функции
             double y = (4.26 * x) / denominator;
             return Math.Round(y, 3); // Округление до 3 знаков
